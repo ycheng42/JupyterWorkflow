fd46f0e6034a007b5960410f95871c388508dc24
Terry Cheng <terrycheng@ip-192-168-86-26.us-west-2.compute.internal>
2017-08-19 21:36:07 -0700

    Mode data download to jupyterworkflow package

---
+++ jupyterWorkflow/data.py
@@ -0,0 +1,29 @@
+import os
+from urllib.request import urlretrieve
+import pandas as pd
+
+FREMONT_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+
+def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL, force_download=False):
+    """Download and cache the fremont data
+    
+    Parameters
+    ----------
+    filename : string (optional)
+        location to save the data
+    url : string (optional)
+        web location of the data
+    force_download : bool (optional)
+        if True, force redownload of data
+
+    Returns
+    -------
+    data : pandas.DataFrame
+        the fremont bridge datal
+    """
+    if force_download or not os.path.exists(filename):
+        urlretriev(url, filename)
+    data = pd.read_csv('Fremont.csv', index_col='Date', parse_dates=True)
+    data.columns = ['West', 'East']
+    data['Total'] = data['West'] + data['East']
+    return data
